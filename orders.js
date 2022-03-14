6d8f924cd0ec1043a4047df0a3aae782fc7613ae
Roo3000 <71338247+Roo3000@users.noreply.github.com>
2022-03-14 10:22:46 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName = 'Foldable Mini Drone',
+        ProductNumber = '85631',
+        payementStatus: 'Due',
+        shipping:'pending'
+    },
+    {
+        productName = 'LAVENDER KF102 Drone',
+        ProductNumber = '36378',
+        payementStatus: 'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName = 'Foldable Mini Drone',
+        ProductNumber = '49347',
+        payementStatus: 'Due',
+        shipping:'pending'
+    },
+    {
+        productName = 'Foldable Mini Drone',
+        ProductNumber = '96996',
+        payementStatus: 'paid',
+        shipping:'Delivered'
+    },
+    {
+        productName = 'GPS 4K Drone',
+        ProductNumber = '22821',
+        payementStatus: 'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName = 'Lozenge Drone',
+        ProductNumber = '00482',
+        payementStatus: 'paid',
+        shipping:'delivered'
+    }
+]
